56a465973714180476fe14b0837db0fbc820b7a6
Gustavo Massaneiro <gustavomassa123@gmail.com>
2020-06-26 00:58:20 -0300

    Added 6 wheels per tank track.

--- Source/BattleTank/BattleTankGameModeBase.cpp
+++ Source/BattleTank/BattleTankGameModeBase.cpp
@@ -1,5 +1,35 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
-
 #include "BattleTankGameModeBase.h"
+#include "Tank/Tank.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
 
+void ABattleTankGameModeBase::BeginPlay()
+{
+    Super::BeginPlay();
+
+    TSubclassOf<ATank> ClassToFind = ATank::StaticClass();
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), ClassToFind, EnemyTankList);
+    PlayerTank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+}
+
+void ABattleTankGameModeBase::HandleGameStart()
+{
+}
+
+void ABattleTankGameModeBase::HandleGameOver(bool PlayerWon)
+{
+}
+
+void ABattleTankGameModeBase::ActorDied(AActor *DeadActor)
+{
+}
+
+void ABattleTankGameModeBase::GameStart()
+{
+}
+
+void ABattleTankGameModeBase::GameOver(bool PlayerWon)
+{
+}
--- Source/BattleTank/BattleTankGameModeBase.h
+++ Source/BattleTank/BattleTankGameModeBase.h
@@ -6,6 +6,8 @@
 #include "GameFramework/GameModeBase.h"
 #include "BattleTankGameModeBase.generated.h"
 
+// Forward Declarations
+class ATank;
 /**
  * 
  */
@@ -13,5 +15,18 @@ UCLASS()
 class BATTLETANK_API ABattleTankGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+public:
+	void ActorDied(AActor *DeadActor);
+	void GameStart();
+	void GameOver(bool PlayerWon);
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	ATank *PlayerTank{nullptr};
+	TArray<AActor *> EnemyTankList;
+
+	void HandleGameStart();
+	void HandleGameOver(bool PlayerWon);
 };
